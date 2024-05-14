19a540dfc2cb9150f98c09ec42b1c922baeb4447
Auden Woolfson <awoolfson@Audens-MacBook-Pro.local>
2024-05-13 17:24:20 -0700

    Router unit tests enhancements

--- presto-main/src/test/java/com/facebook/presto/server/TestQuerySessionSupplier.java
+++ presto-main/src/test/java/com/facebook/presto/server/TestQuerySessionSupplier.java
@@ -14,6 +14,7 @@
 package com.facebook.presto.server;
 
 import com.facebook.presto.Session;
+import com.facebook.presto.common.MockHttpServletRequest;
 import com.facebook.presto.common.WarningHandlingLevel;
 import com.facebook.presto.common.type.TimeZoneNotSupportedException;
 import com.facebook.presto.execution.warnings.WarningCollectorFactory;
